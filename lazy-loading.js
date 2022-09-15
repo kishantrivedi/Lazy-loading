6be623c276428babf3f206e4be911c8117fe5469
kishantrivedi <khtrivedi@scu.edu>
2022-09-15 14:12:56 -0700

    Added lazy-loading for background images

--- lazy-loading.js
+++ lazy-loading.js
@@ -1,5 +1,5 @@
-// Target element
-const target = document.querySelectorAll("img");
+// Target elements
+const target = document.querySelectorAll(".lazy, div.lazyBackground");
 
 // Options for observer
 // Threshold: It is used to determine how much percentage of the element is in the viewport.
@@ -7,22 +7,30 @@ const target = document.querySelectorAll("img");
 //              between the element and viewport is given in px or %
 const options = {
     threshold:1,
-    // rootMargin: "200px"
+    // rootMargin: "500px"
 }
 
 // Creating the observer
 var observer = new IntersectionObserver((elements, observer) => {
     elements.forEach((element) => {
-        if (element.isIntersecting) {       // Checking if the element is intersecting or not.
-            var img = element.target;
+    if (element.isIntersecting) {                       // Checking if the element is intersecting or not.
+        var img = element.target;
+        if (img.classList.contains("lazyBackground")) {
+            path = img.getAttribute('data-src');
+            img.style.backgroundImage = "url(\""+path+"\")"
+            };
+        if (img.classList.contains("lazy")) {
+            img.classList.remove("lazy");
             img.setAttribute('src', img.getAttribute('data-src'));
-            img.parentNode.classList.remove('preloading')
-            img.classList.add('animate-img')
-            observer.unobserve(img);        // This will stop observing the element (image) after it is loaded.
+            img.parentNode.classList.remove('preloading');
+        };
+        img.classList.add('animate-img');
+        img.removeAttribute('data-src');
+        observer.unobserve(img);                   // This will stop observing the element (image) after it is loaded.
         }
     });
-}, options);
+  }, options);
 
-target.forEach (ele => {
-    observer.observe(ele);              // Elements are observed using this method..
+  target.forEach (ele => {
+  observer.observe(ele);                     // Elements are observed using this method.
 })
